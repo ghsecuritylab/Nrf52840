24a83552c02aa2baa1492dfe6589711fa3723b3f
ZhangJianLei <865020911@qq.com>
2020-03-03 22:26:02 +0800

    协议栈模板

--- code/app/main.c
+++ code/app/main.c
@@ -1,17 +1,45 @@
 #include <stdbool.h>
 #include <stdint.h>
+#include <string.h>
+
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
+
 #include "nrf_delay.h"
 #include "boards.h"
 
+/*初始化日志打印模块*/
+static void log_init(void)
+{
+	ret_code_t err_code = NRF_LOG_INIT(NULL);		/*初始化*/
+    APP_ERROR_CHECK(err_code);
+	
+    NRF_LOG_DEFAULT_BACKENDS_INIT();				/*设置log输出终端*/
+}
+
+/*空闲状态处理函数。处理挂起的日志*/
+static void idle_state_handle(void)
+{
+    /*处理挂起的log*/
+	  if (NRF_LOG_PROCESS() == false)
+    {
+
+    }
+}
+
 int main(void)
 {
-	nrf_gpio_cfg_output(LED_2);
+	log_init();
+	nrf_gpio_cfg_output(LED_3);
 	
 	while(1)
 	{
-		nrf_gpio_pin_set(LED_2);
+		NRF_LOG_INFO("BLE Template example:Log use uart as output terminal");
+		nrf_gpio_pin_set(LED_3);
 		nrf_delay_ms(2000);
-		nrf_gpio_pin_clear(LED_2);
+		nrf_gpio_pin_clear(LED_3);
 		nrf_delay_ms(2000);
+		idle_state_handle();
 	}
 }
---
+++ project/RTE/_flash_s140_nrf52_7.0.1_softdevice/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Template' 
+ * Target:  'flash_s140_nrf52_7.0.1_softdevice' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "nrf.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
